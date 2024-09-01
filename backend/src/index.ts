8d2febdf80f2fdae012409ca86b5324f9103006d
Pranab Kalita <pavankalita@gmail.com>
2024-09-01 22:16:21 +0530

    Initial Commit - Setup Express Server with Typescript

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/api/test", async (req: Request, res: Response) => {
+  res.json({ message: "Hello From Express." });
+});
+
+app.listen(7000, () => {
+  console.log("Server running on localhost:7000");
+});
